cc91afea8ee142356f438d74e764a6c3a88dfece
levihedges <60758625+levihedges@users.noreply.github.com>
2020-04-07 01:21:01 +0100

    ASCII art title + sending anomalies to an Elastic index

--- ProjectCode/EmailAnalysisTool.py
+++ ProjectCode/EmailAnalysisTool.py
@@ -2,6 +2,7 @@ from DataAnalysis import DataAnalysis
 from DataLoader import DataLoader
 import argparse
 import datetime
+import hashlib
 import pandas as pd
 
 def main():
@@ -23,7 +24,9 @@ def main():
     dl = DataLoader(index, size, node)
     elastic_response = dl.fetch_data()
     df = dl.create_dataframe(elastic_response)
-    
+
+    #Processes dataframe so that the model has numerical values to work with
+    #Also targets columns more likely to be useful in analysis
     processed_df = df.drop(['@timestamp', 'ID', 'host', 'Content', '@version', 'path', 'message'], axis=1)
     processed_df[['Day', 'Time']] = df.Date.str.split(" ",expand=True,)
     processed_df = processed_df.drop(['Date'], axis=1)
@@ -33,13 +36,28 @@ def main():
     processed_df = pd.get_dummies(processed_df, columns=['Bcc', 'Cc', 'PC', 'To', 'Attachments', 'From', 'User', 'Activity', 'Day', 'Hour'], prefix=['Bcc', 'Cc', 'PC', 'To', 'Attachments', 'From', 'User', 'Activity', 'Day', 'Hour'])
     processed_df = processed_df.apply(pd.to_numeric)
 
-    print(processed_df.dtypes)
-    print(processed_df)
+    #Hash functions so that original events can be retrieved after analysis
+    for index, row in processed_df.iterrows():
+        row_signature = hashlib.md5(row.to_string().encode()).hexdigest()
+        df.at[index, 'Signature'] = row_signature
 
+    values_list = []
     da = DataAnalysis(processed_df)
     values = da.analyse()
-    print(values)
+    for index, row in values.iterrows():
+        row_signature = hashlib.md5(row.to_string().encode()).hexdigest()
+        for index, row in df.iterrows():
+            if row_signature == row['Signature']:
+                values_list.append(row)
 
+    values_df = pd.DataFrame(values_list)
+    print(values_df)
+    print(values_df['User'])
+
+    curr_date = str(datetime.datetime.now())
+    curr_date = curr_date.replace(" ", "")
+
+    dl.send_to_elastic(values_df, curr_date)
 
 def day_update(dataframe):
     for index, row in dataframe.iterrows():
@@ -49,17 +67,20 @@ def day_update(dataframe):
 
     return dataframe
 
-
+#ASCII art title to display in the terminal
 def create_title():
-    print("PLACEHOLDER FOR EMAIL ANALYSIS TOOL TITLE \n")
-
+    print(" ___              _  _  ___            _           _      ___           _")
+    print("| __>._ _ _  ___ <_>| || . |._ _  ___ | | _ _  ___<_> ___|_ _|___  ___ | |")
+    print("| _> | ' ' |<_> || || ||   || ' |<_> || || | |<_-<| |<_-< | |/ . \/ . \| |")
+    print("|___>|_|_|_|<___||_||_||_|_||_|_|<___||_|`_. |/__/|_|/__/ |_|\___/\___/|_|")
+    print("                                         <___'                            \n")
 
 def get_arguments():
     parser = argparse.ArgumentParser(prog="Email Analysis Tool")
     parser.add_argument("-i", "--index", help="Sets the Elasticsearch index that will be queried for data. Default value of 'email' will be used if no index is specified.")
     parser.add_argument("-s", "--size", type=int, help="Sets the number of Elasticsearch documents that will be requested through the API. This needs to be an integer value. Also be aware of the limitations of the system you are running the program on. Any documents that it receives back from your Elasticsearch instance will be added to an in-memory object, so if this value is set too high you will suffer a memory leak. Default value of 10 will be used if no size is specified.")
     parser.add_argument("-n", "--node", help="Sets the URI of the Elasticsearch instance that the request for documents will be sent to. Should contain the domain/IP address and port of the instance, e.g. 'http://localhost:9200' or 'http://127.0.0.1:9200'. Default value of 'http://localhost:9200' will be used if no node is specified.")
-    
+
     return parser.parse_args()
 
 main()
